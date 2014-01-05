9cbd3792186d1ad76ad78e4a2feb480c6a151f54
Joseph Parrinello <joe.parrinello@gmail.com>
2014-01-05 12:40:14 -0600

    Renamed Variables
    Renamed Variables to make for more clear reading.

--- PiRunner.java
+++ PiRunner.java
@@ -1,25 +1,23 @@
-//package com.g1tab.piProgram;
-
 import javax.swing.JFrame;
 import javax.swing.JProgressBar;
 import java.util.ArrayList;
 
 public class PiRunner{
-	private JProgressBar jimmy;
-	private JFrame steve;
+	private JProgressBar sliceProgressBar;
+	private JFrame progressBarFrame;
 
 	public PiRunner(){
 		
 	}
 	public PiRunner(long Slices, double Increments, int Threads){
-		steve= new JFrame();
-		steve.setSize(500,125);
-		jimmy= new JProgressBar(0,(int)Slices);
-		jimmy.setString("Start!");
-		jimmy.setStringPainted(true);
-		steve.add(jimmy);
-		steve.setVisible(true);
-		steve.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		progressBarFrame= new JFrame();
+		progressBarFrame.setSize(500,125);
+		sliceProgressBar= new JProgressBar(0,(int)Slices);
+		sliceProgressBar.setString("Start!");
+		sliceProgressBar.setStringPainted(true);
+		progressBarFrame.add(sliceProgressBar);
+		progressBarFrame.setVisible(true);
+		progressBarFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ArrayList<PiThread> threading= new ArrayList<PiThread>();
 		for (int threads=0; threads<Threads; threads++){
 			threading.add(new PiThread(threads*Increments*Slices/Threads,(threads+1)*Increments*Slices/Threads, Increments, this));
@@ -38,11 +36,11 @@ public class PiRunner{
 			TotArea+=threading.get(threads).getArea();
 		}
 		System.out.println(TotArea);
-		jimmy.setString("DONE!     "+TotArea);
+		sliceProgressBar.setString("DONE!     "+TotArea);
 	}
 	public synchronized void increasePercent(){
-		jimmy.setValue(jimmy.getValue()+1);
-		jimmy.setString("Slices: "+jimmy.getValue());
+		sliceProgressBar.setValue(sliceProgressBar.getValue()+1);
+		sliceProgressBar.setString("Slices: "+sliceProgressBar.getValue());
 	}
 	public static void main(String[] args){
 		new PiRunner(Long.parseLong(args[0]), Math.pow(Long.parseLong(args[0]),-1),Integer.parseInt(args[1]));
--- PiThread.java
+++ PiThread.java
@@ -1,22 +1,21 @@
-//package com.g1tab.piProgram;
-
 import java.util.ArrayList;
+
 public class PiThread extends Thread{
 	private double Area;
-	private double Sta;
-	private double Sto;
-	private double Inc;
+	private double Start;
+	private double Stop;
+	private double Increment;
 	private PiRunner superClass;
 	public PiThread(double start, double stop, double incr, PiRunner supr){
-		Sta=start;
-		Sto=stop;
-		Inc=incr;
+		Start=start;
+		Stop=stop;
+		Increment=incr;
 		superClass=supr;
 	
 	}
 	public void run(){
-		for(double x=Sta; x<Sto; x+=Inc){
-			Area+=Math.sqrt(1-Math.pow(x,2))*Inc*4;
+		for(double x=Start; x<Stop; x+=Increment){
+			Area+=Math.sqrt(1-Math.pow(x,2))*Increment*4;
 			superClass.increasePercent();
 		}
 
